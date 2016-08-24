9a0357b849e8cb8c545cb49cae2e8482efd2d375
aleri0n <aleri0nv0rt3x@gmail.com>
2016-08-24 13:00:57 -0400

    Update hackedornot.py

--- hackedornot.py
+++ hackedornot.py
@@ -1,67 +1,13 @@
 #!/usr/bin/env python
-import requests, json, os
+import requests
+import json 
+import os
 
-print ''
-print ''
-print '  |    |         W3LC0M3           |    |  '
-print '  |    |                           |    |  '
-print ' .........................................'
-print '|                                         |'
-print '|    HackedORNot                          |'
-print '|           v1.1                          |'
-print '|                           By Aleri0n    |'
-print '|                                         |'
-print ' .........................................'
-print '  |    |                           |    |'
-print '  |    |         W3LC0M3           |    |'
-print ''
-print ''
-
-email = raw_input('Enter your email: ')
-if os.name == "posix":
-        os.system('clear')
-        print ''
-        print ''
-        print '  |    |  PL3@S3 W@1T... L0@D1NG.  |    |  '
-        print '  |    |                           |    |  '
-        print ' .........................................'
-        print '|                                         |'
-        print '|    HackedORNot                          |'
-        print '|           v1.1                          |'
-        print '|                           By Aleri0n    |'
-        print '|                                         |'
-        print ' .........................................'
-        print '  |    |                           |    |'
-        print '  |    |  PL3@S3 W@1T... L0@D1NG.  |    |'
-        print ''
-        print ''
-
-elif os.name in ("nt", "dos", "ce"):
-        os.system('CLS')
-        print ''
-        print ''
-        print '  |    |  PL3@S3 W@1T... L0@D1NG.  |    |  '
-        print '  |    |                           |    |  '
-        print ' .........................................'
-        print '|                                         |'
-        print '|    HackedORNot                          |'
-        print '|           v1.1                          |'
-        print '|                           By Aleri0n    |'
-        print '|                                         |'
-        print ' .........................................'
-        print '  |    |                           |    |'
-        print '  |    |  PL3@S3 W@1T... L0@D1NG.  |    |'
-        print ''
-        print ''
-
-else:
-    print '\n' * numlines
-
-
+email = raw_input('Enter your email: ') 
+os.system('clear')
 payload = {'q': email}
 r = requests.get('https://hacked-emails.com/api', params=payload)
 result = json.loads(r.text)
-
 if r.status_code == 200:
    if result['status'] == 'found':
          print 'Warrning! Your email is', result['status'],'!'
@@ -80,16 +26,8 @@ if r.status_code == 200:
             print 'Not verified. Could be false positive...'         
          print ''
          print 'Leak available on:', result['data'][0]['source_network']
-         print ''
-         print ''
-if r.status_code == 200:
-    if result['status'] == 'notfound':
+   elif result['status'] == 'notfound':
          print 'Congratulations! Found no result for: ',email
-         print ''
-         print ''
 else:
          print 'An error occured. Something is wrong...'
          print 'Have you typed the email correctly? :('
-         print ''
-         print ''
- 
